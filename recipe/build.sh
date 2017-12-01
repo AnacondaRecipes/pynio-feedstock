6e28cfe775990615ad1a7418b8fa9f6021ee9927
Michael Sarahan <msarahan@gmail.com>
2017-12-01 07:11:55 -0600

    add cxx and fortran compilers, clean up unnecessary flag stuff

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,18 +12,6 @@ export HAS_HDF4=1
 export HAS_HDF5=1
 export HAS_GDAL=1
 
-export CXXFLAGS="-fPIC $CXXFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export CFLAGS="-D_BSD_SOURCE -D_XOPEN_SOURCE -I$PREFIX/include $CFLAGS"
-
-if [[ $(uname) == Darwin ]]; then
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-  export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-  export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
-fi
+export CFLAGS="$CFLAGS -D_BSD_SOURCE -D_XOPEN_SOURCE"
 
 $PYTHON setup.py install
